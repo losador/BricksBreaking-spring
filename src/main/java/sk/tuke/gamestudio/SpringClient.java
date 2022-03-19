8cbadab354e11ebee889bf97750d1e6d7c95178f
Aleksey Kobel <aleksey.kobel@student.tuke.sk>
2022-03-20 00:54:31 +0300

    add rest

--- src/main/java/sk/tuke/gamestudio/SpringClient.java
+++ src/main/java/sk/tuke/gamestudio/SpringClient.java
@@ -6,13 +6,18 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.client.RestTemplate;
 import sk.tuke.gamestudio.game.consoleUI.ConsoleUI;
 import sk.tuke.gamestudio.game.core.Field;
 import sk.tuke.gamestudio.service.*;
 
 @SpringBootApplication
 @Configuration
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX,
+        pattern = "sk.tuke.gamestudio.server.*"))
 public class SpringClient {
 
     public static void main(String[] args) {
@@ -36,16 +41,22 @@ public class SpringClient {
 
     @Bean
     public ScoreService scoreService() {
-        return new ScoreServiceJPA();
+        return new ScoreServiceRestClient();
     }
 
     @Bean
     public CommentService commentService() {
-        return new CommentServiceJPA();
+        return new CommentServiceRestClient();
     }
 
     @Bean
     public RatingService ratingService() {
-        return new RatingServiceJPA();
+        return new RatingServiceRestClient();
     }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
